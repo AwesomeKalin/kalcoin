25c9ed5a66ba192f26b41a9ce40b130b26ba6d2f
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2023-05-05 19:36:30 +0100

    As many transports as possible

--- src/classes/p2p.ts
+++ src/classes/p2p.ts
@@ -2,7 +2,10 @@ import { noise } from "@chainsafe/libp2p-noise";
 import { tcp } from "@libp2p/tcp";
 import { Libp2p, createLibp2p } from "libp2p";
 import { mplex } from '@libp2p/mplex';
-import { kadDHT } from '@libp2p/kad-dht'
+import { kadDHT } from '@libp2p/kad-dht';
+import { webSockets } from '@libp2p/websockets';
+import { webTransport } from '@libp2p/webtransport';
+import { webRTC } from '@libp2p/webrtc';
 
 export class p2p {
     //@ts-expect-error
@@ -10,7 +13,7 @@ export class p2p {
 
     constructor() {
         var node: any = createLibp2p({
-            transports: [tcp()],
+            transports: [tcp(), webSockets(), webTransport(), webRTC()],
             connectionEncryption: [noise()],
             streamMuxers: [mplex()],
             dht: [kadDHT()],
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import yargs from 'yargs';
+import { CryptoBlockchain } from './classes/CryptoBlockchain.js';
+import { p2p } from './classes/p2p.js';
 
 const argv = yargs(process.argv.slice(2)).options({
     mine: { type: 'boolean', description: 'Whether you want to be able to mine or not', default: false }
 }).parseSync();
 
+let blockchain: CryptoBlockchain = new CryptoBlockchain();
+const peer: p2p = new p2p();
